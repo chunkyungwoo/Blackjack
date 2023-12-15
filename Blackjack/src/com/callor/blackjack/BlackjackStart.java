6e7e1177a785631d96169df7b589600c21c21f96
unknown <ckw2434@naver.com>
2023-12-15 12:51:52 +0900

    black

--- Blackjack/src/com/callor/blackjack/BlackjackStart.java
+++ Blackjack/src/com/callor/blackjack/BlackjackStart.java
@@ -1,9 +1,15 @@
 package com.callor.blackjack;
 
+import com.callor.blackjack.service.BlackjackService;
+import com.callor.blackjack.service.impl.BlackjackServiceImplV2;
+
 public class BlackjackStart {
 	
 	public static void main(String[] args) {
 		
+		BlackjackService blService = new BlackjackServiceImplV2();
+		
+		blService.Dealer();
 	}
 
 }
--- Blackjack/src/com/callor/blackjack/service/impl/BlackjackServiceImplV2.java
+++ Blackjack/src/com/callor/blackjack/service/impl/BlackjackServiceImplV2.java
@@ -6,6 +6,8 @@ public class BlackjackServiceImplV2 extends BlackjackServiceImplV1{
 	public BlackjackServiceImplV2() {
 		
 	}
+	private int userSum = 0;
+	private int dealerSum = 0;
 	// 한장은 보여주고 한장은 보여주지않음
 		// 플레이어가 Hit , Stay 결정시 딜러와 카드비교하고
 		// 16이하면 한장뽑음 17 이상이면 자동스테이
@@ -14,24 +16,27 @@ public class BlackjackServiceImplV2 extends BlackjackServiceImplV1{
 		// ace가 1 또는 11 이기때문에 딜러는 추가로 히트로 진행할수있다
 	@Override
 	public void Dealer() {
-		Integer cardSet = 0;
+		int cardSet = 0;
+		int count = 0;
 		Line.dLine(50);
 		System.out.println("블랙잭 게임 시작");
 		Line.dLine(50);
 		while(true) {
 			this.card();
-			System.out.print("딜러가 카드를 받습니다");
+			System.out.println("딜러가 카드를 받습니다");
+			System.out.print("HIt or STAY");
 			String str = scan.nextLine();
-			System.out.println("Hit or STAY");
-			if(str.equals("HiT")) {
-				
-			}
+			if(str.equals("HIT")) {
+				System.out.println(cardSet);
+				count++;
+			}else if(str.equals("STAY"))
+				System.out.println();
 			
 		}
 		
 	}
 	public void Player() {
-		// 카드를 한장 뽑을수있게끔 명령어를 생성해야함
+		// hit 카드를 한장 뽑을수있게끔 명령어를 생성해야함
 	}
 
 }
